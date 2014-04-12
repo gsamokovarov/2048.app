90a2294aeff28d3d16da3bd175681c1432198604
Genadi Samokovarov <gsamokovarov@gmail.com>
2014-04-12 20:06:26 +0200

    Extract the menu creation and add debug support

--- js/application.js
+++ js/application.js
@@ -1,11 +1,18 @@
 var gui = require ('nw.gui');
 
-function Application() {
+function Application(desktopWindow) {
+  this.desktopWindow = desktopWindow || gui.Window.get();
+  this.gameManager   = new GameManager(4, KeyboardInputManager, HTMLActuator, LocalStorageManager);
+
+  this.initializeMenu();
+}
+
+Application.prototype.initializeMenu = function () {
   var menubar = new gui.Menu({ type: 'menubar' });
 
   // See https://github.com/rogerwang/node-webkit/issues/743 to get an idea of
   // of how to get the ordering right.
-  gui.Window.get().menu = menubar;
+  this.desktopWindow.menu = menubar;
 
   var gameMenu = new gui.Menu;
   gameMenu.append(new gui.MenuItem({
@@ -17,7 +24,7 @@ function Application() {
 
   gameMenu.append(new gui.MenuItem({
     label: 'Quit',
-    click: function() { window.close() }
+    click: function() { window.close(); }
   }));
 
   menubar.insert(new gui.MenuItem({
@@ -25,11 +32,25 @@ function Application() {
     submenu: gameMenu
   }), 1);
 
-  this.gameManager = new GameManager(4, KeyboardInputManager, HTMLActuator, LocalStorageManager);
-}
+  if (process.env.DEBUG) {
+    var debugMenu = new gui.Menu;
 
-Application.run = function () {
-  window.requestAnimationFrame(function () { new Application; });
-}
+    menubar.append(new gui.MenuItem({
+      label: 'Debug',
+      submenu: debugMenu
+    }));
+
+    debugMenu.append(new gui.MenuItem({
+      label: 'Developer Tools',
+      click: function() { gui.Window.get().showDevTools(); }
+    }));
+  }
+};
+
+Application.run = function (desktopWindow) {
+  window.requestAnimationFrame(function () {
+    new Application(desktopWindow);
+  });
+};
 
 Application.run();
