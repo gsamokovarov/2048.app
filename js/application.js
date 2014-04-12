10fc730470b2ae44f5930564f998be1d4b500240
Genadi Samokovarov <gsamokovarov@gmail.com>
2014-04-12 15:06:33 +0200

    Add a Game menu
    It can start new game and quit.

--- js/application.js
+++ js/application.js
@@ -1,4 +1,33 @@
-// Wait till the browser is ready to render the game (avoids glitches)
-window.requestAnimationFrame(function () {
-  new GameManager(4, KeyboardInputManager, HTMLActuator, LocalStorageManager);
-});
+var gui = require ('nw.gui');
+
+function Application() {
+  var menubar = new gui.Menu({ type: 'menubar' });
+
+  // See https://github.com/rogerwang/node-webkit/issues/743 to get an idea of
+  // of how to get the ordering right.
+  gui.Window.get().menu = menubar;
+
+  var gameMenu = new gui.Menu;
+  gameMenu.append(new gui.MenuItem({
+    label: 'New Game',
+    click: function() { this.gameManager.restart(); }.bind(this)
+  }));
+
+  gameMenu.append(new gui.MenuItem({
+    label: 'Quit',
+    click: function() { window.close() }
+  }));
+
+  menubar.insert(new gui.MenuItem({
+    label: 'Game',
+    submenu: gameMenu
+  }), 1);
+
+  this.gameManager = new GameManager(4, KeyboardInputManager, HTMLActuator, LocalStorageManager);
+}
+
+Application.run = function () {
+  window.requestAnimationFrame(function () { new Application; });
+}
+
+Application.run();
